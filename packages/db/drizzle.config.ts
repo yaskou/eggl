7bb04c9b205073450a91acbdb4ea550e02781ba3
yaskou <anmitutarow@gmail.com>
2026-01-07 16:42:07 +0900

    chore: change the database to turso

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -4,8 +4,9 @@ import { defineConfig } from "drizzle-kit";
 export default defineConfig({
   out: "./drizzle",
   schema: "./src/schemas/**/*.ts",
-  dialect: "sqlite",
+  dialect: "turso",
   dbCredentials: {
-    url: process.env.DB_FILE_NAME!,
+    url: process.env.TURSO_CONNECTION_URL!,
+    authToken: process.env.TURSO_AUTH_TOKEN!,
   },
 });
--- packages/db/src/db.ts
+++ packages/db/src/db.ts
@@ -2,4 +2,10 @@ import "dotenv/config";
 import { drizzle } from "drizzle-orm/libsql";
 import * as schema from "./schemas";
 
-export const db = drizzle(process.env.DB_FILE_NAME!, { schema });
+export const db = drizzle({
+  connection: {
+    url: process.env.TURSO_CONNECTION_URL!,
+    authToken: process.env.TURSO_AUTH_TOKEN!,
+  },
+  schema,
+});
